5cc158d5a96c230658ff13203259b282340de292
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2020-01-20 18:34:00 -0500

    [ADD] Added target create test

--- observation-management-api/app/observations_manager/tests.py
+++ observation-management-api/app/observations_manager/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from observations_manager.models import Target
 
-# Create your tests here.
+
+class TestTargetCase(TestCase):
+    def setUp(self):
+        Target.objects.create(
+            name="Target 1", coordinates="POINT(-15.232322 36.5656)", elevation=500)
+
+    def test_target_was_created(self):
+        target = Target.objects.get(name="Target 1")
+        self.assertEqual(target.elevation, 500, msg=None)
