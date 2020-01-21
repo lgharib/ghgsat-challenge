58196bbc18dee4a9b4c597663c014748b578e251
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2020-01-20 19:09:42 -0500

    [ADD] Added Target model and fixed Docker issues

--- observation-management-api/app/observationmanagementapi/settings.py
+++ observation-management-api/app/observationmanagementapi/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'rest_framework',
+    'observations_manager'
 ]
 
 MIDDLEWARE = [
--- observation-management-api/app/observations_manager/models.py
+++ observation-management-api/app/observations_manager/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Target(models.Model):
+    name = models.CharField(max_length=50)
+    coordinates = models.PointField(null=True, blank=True)
+    elevation = models.IntegerField()
+
+    def __str__(self):
+        return self.name
